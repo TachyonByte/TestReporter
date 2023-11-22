bc691ae00dd5ef6b99aeec94135def7b9e2e81e5
Tanmay More <tanmay.18519@gmail.com>
2023-11-22 13:15:27 +0530

    docstring added.

--- projectreporter/cases/models.py
+++ projectreporter/cases/models.py
@@ -4,6 +4,18 @@ from projects.models import System, Feature, Project
 
 
 class TestCase(models.Model):
+    """
+    Model to store information about test cases.
+
+    Attributes:
+        system (ForeignKey): The system that test relates to.
+        feature (ForeignKey): The feature that test relates to.
+        name (CharField): Name of test case.
+        description (TextField): Info of the test case.
+        created_by (ForeignKey): Creator of the test case.
+        created_at (DateTimeField): Test created on(date and time).
+        updated_at (DateTimeField): Test updated on(date and time).
+    """
     system = models.ForeignKey(System, on_delete=models.CASCADE)
     feature = models.ForeignKey(Feature, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
@@ -17,6 +29,17 @@ class TestCase(models.Model):
 
 
 class ChildTestCase(models.Model):
+    """
+    Model to store sub tests of TestCase instances.
+
+    Attributes:
+        parent (ForeignKey): Testcase that this test is related to.
+        name (CharField): Name of the test.
+        description (TextField): information of the test.
+        created_by (ForeignKey): Creator of the test case.
+        created_at (DateTimeField): Test created on(date and time).
+        updated_at (DateTimeField): Test updated on(date and time).
+    """
     parent = models.ForeignKey(TestCase, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
--- projectreporter/projects/models.py
+++ projectreporter/projects/models.py
@@ -3,14 +3,29 @@ from django.contrib.auth.models import User
 
 
 class Project(models.Model):
-    """Project Model"""
+    """
+    Model to store information about Project.
+
+    Attributes:
+        name (CharField): Name of Project.
+        description (TextField): Info of the project.
+        created_at (DateTimeField): Project created on(date and time).
+    """
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class System(models.Model):
-    "System model for apps in a project"
+    """
+    Model to store information about particular system of project.
+
+    Attributes:
+        project (ForeignKey): The project that system relates to.
+        name (CharField): Name of system.
+        description (TextField): Info of the system.
+        created_at (DateTimeField): system created on(date and time).
+    """
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
@@ -18,6 +33,17 @@ class System(models.Model):
 
 
 class Feature(models.Model):
+    """
+    Model to store information about particular feature of system related
+    to project.
+
+    Attributes:
+        project (ForeignKey): The project that feature relates to.
+        name (CharField): Name of feature.
+        description (TextField): Info of the feature.
+        created_at (DateTimeField): feature created on(date and time).
+        created_by (ForeignKey): Creator of feature.
+    """
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
