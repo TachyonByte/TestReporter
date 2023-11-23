697e2b02910f41d9aaf6d80cd6766fb3073b3624
Devyani <raidevyani3@gmail.com>
2023-11-23 18:02:56 +0530

    views rendered

--- projectreporter/reports/urls.py
+++ projectreporter/reports/urls.py
@@ -3,22 +3,46 @@ from django.urls import path
 from .views import (ProjectCreateView, ProjectListView,
                     ProjectDetailView, ProjectUpdateView, SystemCreateView,
                     SystemListView, SystemUpdateView,
-                    FeatureCreateView, FeatureListView, FeatureUpdateView)
+                    FeatureCreateView, FeatureListView, FeatureUpdateView,
+                    TestCaseCreateView, TestCaseDetailView, TestCaseListView,
+                    TestCaseUpdateView, ReportCreateView, ReportDetailView,
+                    ReportListView, ReportUpdateView, TestResultCreateView,
+                    TestResultDetailView, TestResultListView, TestResultUpdateView)
 
 app_name = "reports"
 
 urlpatterns = [
-    # path('', HomePageView.as_view(), name='home'),
+    # Project Urls
     path('new-project/', ProjectCreateView.as_view(), name='new-project'),
     path('', ProjectListView.as_view(), name='projects'),
     path('project/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
     path('Updatde-project/<int:pk>/', ProjectUpdateView.as_view(), name='update-project'),
 
-    path('new-system/', SystemCreateView.as_view(), name='new-system'),
+    # System Urls
+    path('new-system/<int:pk>/', SystemCreateView.as_view(), name='new-system'),
     path('systems/', SystemListView.as_view(), name='systems'),
     path('Updatde-project/<int:pk>/', SystemUpdateView.as_view(), name='update-system'),
 
-    path('new-feature/', FeatureCreateView.as_view(), name='new-feature'),
+    # Feature Urls
+    path('new-feature/<int:pk>/<int:spk>/', FeatureCreateView.as_view(), name='new-feature'),
     path('features/', FeatureListView.as_view(), name='features'),
     path('Updatde-feature/<int:pk>/', FeatureUpdateView.as_view(), name='update-feature'),
+
+    # TestCase Urls
+    path('new-testcase/<int:pk>/<int:spk>/', TestCaseCreateView.as_view(), name ="new-testcase"),
+    path('testcases/', TestCaseListView.as_view(), name="testcases"),
+    path('testcase/<int:pk>/', TestCaseDetailView.as_view(), name="testcase-detail"),
+    path('Updatde-testcase/<int:pk>/', TestCaseUpdateView.as_view(), name="update-testcase"),
+
+    # Report Urls
+    path('new-report/', ReportCreateView.as_view(), name='new-report'),
+    path('reports', ReportListView.as_view(), name='reports'),
+    path('project/<int:pk>/', ReportDetailView.as_view(), name='report_detail'),
+    path('Updatde-project/<int:pk>/', ReportUpdateView.as_view(), name='update-report'),
+
+    # TestResult Urls
+    path('new-testresult/', TestResultCreateView.as_view(), name='new-testresult'),
+    path('testresult', TestResultListView.as_view(), name='testresults'),
+    path('testresult/<int:pk>/', TestResultDetailView.as_view(), name='testresult_detail'),
+    path('Updatde-testresult/<int:pk>/', TestResultUpdateView.as_view(), name='update-testresult'),
 ]
--- projectreporter/reports/views.py
+++ projectreporter/reports/views.py
@@ -12,7 +12,8 @@ from reports.models import Report, TestResult
 # class HomePageView(TemplateView):
 #     template_name = "reports/dashboard.html"
 
-class ProjectListView( ListView):
+# Project List, Detail, Create, Update Views
+class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = "reports/dashboard.html"
     context_object_name = "projects"
@@ -35,13 +36,13 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
     context_object_name = "new_project"
     success_url = reverse_lazy('reports:projects')
 
-
 class ProjectUpdateView(LoginRequiredMixin, UpdateView):
     model = Project
     fields = ['name', 'description']
     template_name = "reports/project_update.html"
     context_object_name = "update_project"
 
+# System List, Create, Update Views
 class SystemListView(LoginRequiredMixin, ListView):
     model = System
     template_name = "reports/project_detail.html"
@@ -54,7 +55,10 @@ class SystemCreateView(LoginRequiredMixin, CreateView):
     context_object_name = "new_system"
     success_url = reverse_lazy('reports:projects')
 
-
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['project'] = Project.objects.get(pk=self.kwargs['pk'])
+        return initial
 
 class SystemUpdateView(LoginRequiredMixin, UpdateView):
     model = System
@@ -62,6 +66,7 @@ class SystemUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "reports/project_update.html"
     context_object_name = "update_system"
 
+# Feature List, Create, Update Views
 class FeatureListView(LoginRequiredMixin, ListView):
     model = Feature
     template_name = "reports/pages.html"
@@ -74,8 +79,98 @@ class FeatureCreateView(LoginRequiredMixin, CreateView):
     context_object_name = "new_features"
     success_url = reverse_lazy('reports:projects')
 
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['project'] = Project.objects.get(pk=self.kwargs['pk'])
+        initial['system'] = System.objects.get(pk=self.kwargs['spk'])
+        return initial
+
 class FeatureUpdateView(LoginRequiredMixin, UpdateView):
     model = Feature
     fields = ['name', 'description']
     template_name = "reports/project_update.html"
     context_object_name = "update_features"
+
+# TestCases List, Create, Detail, Update
+
+class TestCaseListView(LoginRequiredMixin, ListView):
+    model = TestCase
+    template_name = "reports/testcases.html"
+    context_object_name = "testcases"
+
+class TestCaseCreateView(LoginRequiredMixin, CreateView):
+    model = TestCase
+    fields = '__all__'
+    template_name = "reports/testcase_create.html"
+    context_object_name = "new_testcases"
+    success_url = reverse_lazy('reports:testcases')
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['feature'] = Feature.objects.get(pk=self.kwargs['pk'])
+        initial['system'] = System.objects.get(pk=self.kwargs['spk'])
+        return initial
+
+class TestCaseDetailView(LoginRequiredMixin, DetailView):
+    model = TestCase
+    template_name = "reports/testcase_detail.html"
+    context_object_name = "testcase_detail"
+    
+
+class TestCaseUpdateView(LoginRequiredMixin, UpdateView):
+    model = TestCase
+    fields = ['name', 'description']
+    template_name = "reports/testcase_update.html"
+    context_object_name = "update_testcases"
+
+# Report List, Create, Detail, Update
+
+class ReportListView(LoginRequiredMixin, ListView):
+    model = Report
+    template_name = "reports/reports.html"
+    context_object_name = "reports"
+
+class ReportCreateView(LoginRequiredMixin, CreateView):
+    model = Report
+    fields = '__all__'
+    template_name = "reports/reports_create.html"
+    context_object_name = "new_reports"
+    success_url = reverse_lazy('reports:testcases')
+
+class ReportDetailView(LoginRequiredMixin, DetailView):
+    model = Report
+    template_name = "reports/reports_detail.html"
+    context_object_name = "reports_detail"
+    
+
+class ReportUpdateView(LoginRequiredMixin, UpdateView):
+    model = Report
+    fields = ['name', 'description']
+    template_name = "reports/reports_update.html"
+    context_object_name = "update_reports"
+
+# TestResult List, Create, Detail, Update
+
+class TestResultListView(LoginRequiredMixin, ListView):
+    model = TestResult
+    template_name = "reports/reports.html"
+    context_object_name = "reports"
+
+class TestResultCreateView(LoginRequiredMixin, CreateView):
+    model = TestResult
+    fields = '__all__'
+    template_name = "reports/reports_create.html"
+    context_object_name = "new_reports"
+    success_url = reverse_lazy('reports:testcases')
+
+class TestResultDetailView(LoginRequiredMixin, DetailView):
+    model = TestResult
+    template_name = "reports/reports_detail.html"
+    context_object_name = "reports_detail"
+    
+
+class TestResultUpdateView(LoginRequiredMixin, UpdateView):
+    model = TestResult
+    fields = ['name', 'description']
+    template_name = "reports/reports_update.html"
+    context_object_name = "update_reports"
